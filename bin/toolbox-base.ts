741aaf9b6e5494a7332f58675cdf08e6179002de
Gavin Dean <gavindean90@gmail.com>
2022-01-05 23:30:36 -0800

    adds ecr

--- bin/toolbox-base.ts
+++ bin/toolbox-base.ts
@@ -3,7 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import { ToolboxBaseStack } from '../lib/toolbox-base-stack';
 
 const app = new cdk.App();
-new MyPipelineStack(app, 'MyPipelineStack', {
+new ToolboxBaseStack(app, 'ToolboxBaseStack', {
   env: {
     account: '822585835475',
     region: 'us-west-2',
--- lib/toolbox-base-stack.ts
+++ lib/toolbox-base-stack.ts
@@ -1,22 +1,7 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
-export class ToolboxBaseStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ToolboxBaseQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
-}
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { ToolboxBaseEcrStage } from './toolbox-base-ecr-stage'
 
 export class ToolboxBaseStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -25,9 +10,12 @@ export class ToolboxBaseStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'ToolboxBase',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+        input: CodePipelineSource.connection('Dean-Family/toolbox-base', 'main', {
+            connectionArn: 'arn:aws:codestar-connections:us-west-2:822585835475:connection/955d3126-4e46-4c16-9e98-cba0fc7f9558',
+        }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
+    pipeline.addStage(new ToolboxBaseEcrStage(this, "test")
   }
 }
