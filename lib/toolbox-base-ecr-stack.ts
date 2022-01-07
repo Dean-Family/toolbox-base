9e5f51800baae75527cfd8ebfd337d7a1ce355cb
Gavin Dean <gavindean90@gmail.com>
2022-01-07 11:59:30 -0800

    updates to use the docker image asset

--- lib/toolbox-base-ecr-stack.ts
+++ lib/toolbox-base-ecr-stack.ts
@@ -1,11 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Repository } from 'aws-cdk-lib/aws-ecr';
+import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
 
 export class ToolboxBaseEcrStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props);
   
-      new Repository(this, 'ContainerImage');
+      //new Repository(this, 'ContainerImage');
+      new DockerImageAsset(this, 'ContainerImage', {
+        directory: path.join(__dirname, 'container')
+      });
     }
 }
